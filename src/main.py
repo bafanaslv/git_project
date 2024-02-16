8cafd429aa8a5ba6fc08c45fa5d723deb89f6032
Вячеслав Лукин <bafana.slv@gmail.com>
2024-02-16 19:22:43 +0300

    Revert "add file main.py"
    This reverts commit b697607abfd1b0adf29d9d976f8a16b2b75c617b.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("Hello Linux!")
+=======
 
+>>>>>>> parent of 0a99fd1 (edit file main.py)
+=======
+
+>>>>>>> parent of 0a99fd1 (edit file main.py)
+=======
+
+>>>>>>> parent of 0a99fd1 (edit file main.py)
+=======
+
+>>>>>>> parent of 0a99fd1 (edit file main.py)

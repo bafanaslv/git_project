39469b8e02ecabef4cdbb66e3668baf527ef5b37
Вячеслав Лукин <bafana.slv@gmail.com>
2024-02-16 19:17:14 +0300

    Revert "edit file main.py"
    This reverts commit 0a99fd1c6cfec524e3bfd04ae0a46d058623df93.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Hello Linux!")
+=======
+
+>>>>>>> parent of 0a99fd1 (edit file main.py)
 =======
 
 >>>>>>> parent of 0a99fd1 (edit file main.py)
